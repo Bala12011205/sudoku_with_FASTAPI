b47c058ba3ada582e0e99798dc60666f70f07d8f
Bala12011205 <natrajbalamurugan@gmail.com>
2026-09-22 16:46:52 +0530

    Code optimization for the loop function

--- sudoku.py
+++ sudoku.py
@@ -13,20 +13,57 @@ def is_valid(board, row, col, num):
                 return False
     return True
 
+# time consuming method so planned to use MRV method 
+# def fill_board(board):
+#     for i in range(9):
+#         for j in range(9):
+#             if board[i][j] == 0:
+#                 numbers = list(range(1, 10))
+#                 random.shuffle(numbers)
+#                 for num in numbers:
+#                     if is_valid(board, i, j, num):
+#                         board[i][j] = num
+#                         if fill_board(board):
+#                             return True
+#                         board[i][j] = 0
+#                 return False
+#     return True
+    
 def fill_board(board):
+    best_cell = None
+    best_numbers = None
+
     for i in range(9):
         for j in range(9):
             if board[i][j] == 0:
-                numbers = list(range(1, 10))
-                random.shuffle(numbers)
-                for num in numbers:
-                    if is_valid(board, i, j, num):
-                        board[i][j] = num
-                        if fill_board(board):
-                            return True
-                        board[i][j] = 0
-                return False
-    return True
+                numbers = [
+                    num for num in range(1, 10)
+                    if is_valid(board, i, j, num)
+                ]
+
+                if not numbers:
+                    return False
+
+                if best_numbers is None or len(numbers) < len(best_numbers):
+                    best_cell = (i, j)
+                    best_numbers = numbers
+
+    # No empty cells
+    if best_cell is None:
+        return True
+
+    i, j = best_cell
+    random.shuffle(best_numbers)
+
+    for num in best_numbers:
+        board[i][j] = num
+
+        if fill_board(board):
+            return True
+
+        board[i][j] = 0
+
+    return False
 
 def generate_puzzle(difficulty_holes=30):
     board = [[0 for _ in range(9)] for _ in range(9)]
